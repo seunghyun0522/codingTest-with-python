d03d61c6e905aa40abc82e2b4672d7f81a94e5f0
seunghyun0522 <cvtcvt007@naver.com>
2025-01-12 21:28:21 +0900

    10817 success

--- codingTest.py
+++ codingTest.py
@@ -1,20 +1,10 @@
-H, M, S = map(int, input().split())
-D = int(input()) 
+a,b,c = input().split()
 
-S += D % 60
-D = D // 60
-if S >= 60:
-    S -= 60
-    M += 1
 
-M += D % 60
-D = D // 60
-if M >= 60:
-    M -= 60
-    H += 1
+a = int(a)
+b = int(b)
+c = int(c)
 
-H += D % 24
-if H >= 24:
-    H -= 24
-    
-print(H,M,S)
+array = [a,b,c]
+array.sort()
+print(array[1])
