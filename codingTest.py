7cbce83e45e87a6c0c84e75b686cf3e5774c0f51
seunghyun0522 <cvtcvt007@naver.com>
2025-01-15 22:25:51 +0900

    1037 success!!

--- codingTest.py
+++ codingTest.py
@@ -1,18 +1,8 @@
-case = int(input())
-answer = 0
 
-for _ in range(case):
-    a, b, c = map(int, input().split())
-    
-    if a == b == c:
-        answer = max(answer, 10000+a*1000)
-    elif a == b:
-        answer = max(answer, 1000+a*100)
-    elif a == c:
-        answer = max(answer, 1000+a*100)
-    elif b == c:
-        answer = max(answer, 1000+b*100)
-    else:
-        answer = max(answer, 100 * max(a,b,c))
+a = int(input())
 
-print(answer)
+b = list(map(int, input().split()))
+
+b.sort()
+
+print(b[0] * b[-1]) 
