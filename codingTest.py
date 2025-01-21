7f9c4735b3e53a5235a45893250f6a8c6d9c7830
seunghyun0522 <cvtcvt007@naver.com>
2025-01-21 19:36:49 +0900

    풍선 터뜨리기 2346  success

--- codingTest.py
+++ codingTest.py
@@ -1,27 +1,22 @@
-# 2
-# 3
-# Yonsei 10
-# Korea 10000000
-# Ewha 20
-# 2
-# Yonsei 1
-# Korea 10000000 
+n = int(input())  # 풍선의 개수
+a = list(map(int, input().split()))  # 풍선의 이동 값 리스트
+indices = list(range(1, n + 1))  # 풍선의 원본 인덱스 (1-based)
+result = []  # 결과를 저장할 리스트
+idx = 0  # 현재 인덱스
 
+while a:
+    move = a[idx]  # 현재 풍선의 이동 값
+    result.append(indices.pop(idx))  # 원본 인덱스를 결과에 추가
+    a.pop(idx)  # 현재 풍선을 제거
 
-T = int(input())
-result = []
-for _ in range(T):
-    max = 0
-    max_univ = ""
-    N = int(input())
-    for _ in range(N):
-        univ , beer = input().split()
-        beer = int(beer)
-        if beer > max :
-            max = beer
-            max_univ = univ
-            
-    result.append(max_univ)
-            
+    if not a:  # 리스트가 비었으면 종료
+        break
 
-print("\n".join(result))
+    # 다음 인덱스 계산
+    if move > 0:
+        idx = (idx + move - 1) % len(a)  # 오른쪽으로 이동
+    else:
+        idx = (idx + move) % len(a)  # 왼쪽으로 이동
+
+result = ' '.join(map(str, result))
+print(result)
