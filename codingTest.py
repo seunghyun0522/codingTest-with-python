5ffa57ec2578a96777639154a08b8da8536eef68
seunghyun0522 <cvtcvt007@naver.com>
2025-01-26 19:36:26 +0700

    2346 review

--- codingTest.py
+++ codingTest.py
@@ -1,16 +1,27 @@
-import math
-m = int(input())
-n = int(input())
-sum = 0 
-min = n
-for i in range(m, n+1):
-    if math.sqrt(i) == int(math.sqrt(i)):
-        sum += i
-        if min > i:
-            min = i
+n = int(input())  
+a = list(map(int, input().split())) 
+idx_list=[]
+idx = 0
+result =[]
+for i in range(n):
+    idx_list.append(i+1)
 
-if sum > 0:
-    print(sum)
-    print(min)
-else:
-    print(-1)
+
+
+while a:
+    move = a[idx]
+    result.append(idx_list.pop(idx))
+    a.pop(idx)
+
+    if not a:
+        break
+    if move > 0:
+        idx = (idx + move -1)%len(a)
+    else:
+        idx = (idx + move)%len(a)
+    
+   
+
+
+result = ' '.join(map(str, result))
+print(result)
