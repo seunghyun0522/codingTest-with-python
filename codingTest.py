f1c8238dad3cbe61d7c88f7e1ee2724d454e0a54
seunghyun0522 <cvtcvt007@naver.com>
2025-01-27 21:21:25 +0700

    이 문제를 해결하고 싶다... 24511

--- codingTest.py
+++ codingTest.py
@@ -1,27 +1,6 @@
-n = int(input())  
-a = list(map(int, input().split())) 
-idx_list=[]
-idx = 0
-result =[]
-for i in range(n):
-    idx_list.append(i+1)
+N = int(input())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+M = int(input())
+C = list(map(int, input().split()))
 
-
-
-while a:
-    move = a[idx]
-    result.append(idx_list.pop(idx))
-    a.pop(idx)
-
-    if not a:
-        break
-    if move > 0:
-        idx = (idx + move -1)%len(a)
-    else:
-        idx = (idx + move)%len(a)
-    
-   
-
-
-result = ' '.join(map(str, result))
-print(result)
