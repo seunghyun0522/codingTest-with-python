e9621c11f9d3b1a82850e82cce7a01e8fe912e37
seunghyun0522 <cvtcvt007@naver.com>
2025-01-28 23:08:12 +0900

    test

--- codingTest.py
+++ codingTest.py
@@ -2,6 +2,6 @@ from datetime import datetime
 
 year = datetime.today().year       
 month=datetime.today().month
-day1=datetime.today().day        
+day2=datetime.today().day        
 
-print("%d-%02d-%02d" % (year, month, day1))
+print("%d-%02d-%02d" % (year, month, day2))
