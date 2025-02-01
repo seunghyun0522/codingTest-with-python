37b4b226cadc00f52482dc60da6ed2638db9b705
seunghyun0522 <cvtcvt007@naver.com>
2025-02-01 22:30:18 +0900

    0201 간단 문제 2개 해결

--- codingTest.py
+++ codingTest.py
@@ -1,17 +1,7 @@
-from collections import deque
-N = int(input())
-A = list(map(int, input().split()))
-B = list(map(int, input().split()))
-M = int(input())
-C = list(map(int, input().split()))
+from datetime import datetime
 
-result = []
+year = datetime.today().year       
+month=datetime.today().month
+day=datetime.today().day        
 
-queue = deque([])
-for i in range(N):
-    if(A[i] ==0):
-        queue.append(B[i])
-
-for i in range(M):
-    queue.appendleft(C[i])
-    print(queue.pop(), end=' ')
+print("%d-%02d-%02d" % (year, month, day))
